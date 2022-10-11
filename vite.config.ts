d56a6886f5efdf486a098fe3b701498215deffef
Tomasz Marczak <tommy.marczak@gmail.com>
2022-10-12 07:32:38 +0800

    Pre deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/roulette-strategy/",
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1600,
     outDir: "./build",
   },
 });
